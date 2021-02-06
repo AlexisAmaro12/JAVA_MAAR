83afcbf69c5b750a9484c2736885166033ee00cb
AlexisAmaro12 <alexisamaro45@aragon.unam.mx>
2021-02-05 18:47:07 -0600

    Ejemplos de variables

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables {
+
+	public static void main (String [] args){
+/**
+*Programa que calcule el area de un cuadrado
+*
+*20 metros
+**/
+		int lado = 20;
+		int area;
+		area = lado * lado;
+		System.out.print("El area es ");
+		System.out.print(area);
+		System.out.print(" metros");
+	}
+}
