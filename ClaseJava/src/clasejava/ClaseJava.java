e899fffce3d8bad58a5a6fb13eae2ead5f40d141
AlexisAmaro12 <alexisamaro45@aragon.unam.mx>
2021-02-05 19:23:30 -0600

    Codigos desde el IDE

---
+++ ClaseJava/src/clasejava/ClaseJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasejava;
+
+/**
+ *
+ * @author spyro
+ */
+public class ClaseJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int lado = 20;
+        int area;
+            area = lado * lado;
+        System.out.print("El area es ");
+        System.out.print(area);
+        System.out.println(" metros");
+    }
+    
+}
---
+++ JavaApplicationApache/src/javaapplicationapache/JavaApplicationApache.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationapache;
+
+/**
+ *
+ * @author spyro
+ */
+public class JavaApplicationApache {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int lado = 20;
+        int area; 
+        area = lado * lado;
+        System.out.print("Tu area es ");
+        System.out.print(area);
+        System.out.println(" metros");
+    }
+    
+}
